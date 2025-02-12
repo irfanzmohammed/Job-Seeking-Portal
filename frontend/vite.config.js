25421164336b9958c88f5fd0681341a49ad24a6d
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-02-12 20:33:29 +0530

    done deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
+// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   base: "./", // Ensures correct asset paths in production
-//   build: {
-//     outDir: "dist", //  Vercel expects built files in "dist"
-//   },
-// });
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist", // Vercel expects built files in the "dist" directory
+    assetsDir: "assets", // Ensure assets are placed in the correct folder
+  },
+});
+
