6d52f3c068fdbb163d333f97c02823e2da32c148
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-02-14 00:10:05 +0530

    fine

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"  // Important for correct asset resolution
+  base: "/"  // Important for correct asset resolution
 })
