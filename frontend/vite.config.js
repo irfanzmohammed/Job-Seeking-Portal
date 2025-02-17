8c9165f4c3c6582730f78f023a481542554d6c27
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-02-17 19:02:03 +0530

    done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // Ensures correct asset paths
+  base: "./",  // Ensures correct asset paths
   build: {
     outDir: "dist",
     assetsDir: "assets",
